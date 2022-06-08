ce9132cba26828935198786b5bde02a9dd98583d
reyangurjar <reyanyashgurjar@gmail.com>
2022-06-08 21:32:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-yagr8x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-yagr8x)
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./main.css";
+import { BrowserRouter } from "react-router-dom";
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
